9e42300b97e899b7406cbd0c30f5cb16ec52c9a1
michael-coles <michael.coles@alertlogic.com>
2017-11-07 13:50:38 +0000

    String keys in map

--- test/ini_test.exs
+++ test/ini_test.exs
@@ -14,24 +14,24 @@ defmodule IniTest do
   test "generic file is key value pairs" do
     {:ok, ini} = File.read "./test/files/generic.ini"
     decoded = Ini.decode ini
-    assert decoded == %{anotherkey: "value", test: "1", yetanother: "another"}
+    assert decoded == %{"anotherkey" => "value", "test" => "1", "yetanother" => "another"}
   end
 
   test "nested maps from ini headers" do
     {:ok, ini} = File.read "./test/files/header.ini"
     decoded = Ini.decode ini
-    assert decoded == %{super_admin_test_header: %{another: "test", host: "http://www.google.com", pass: "test", user: "test"}}
+    assert decoded == %{"super_admin_test_header" => %{"another" => "test", "host" => "http://www.google.com", "pass" => "test", "user" => "test"}}
   end
 
   test "test arrays" do
     {:ok, ini} = File.read "./test/files/arrays.ini"
     decoded = Ini.decode ini
-    assert decoded == %{ arrays_test: %{arr: ["test1", "test2", "test3", "test4"]}}
+    assert decoded == %{ "arrays_test" => %{"arr" => ["test1", "test2", "test3", "test4"]}}
   end
 
   test "test full" do
     {:ok, ini} = File.read "./test/files/full.ini"
     decoded = Ini.decode ini
-    assert decoded == %{body: %{h1: "test", h3: "testing"}, footer: %{js: ["script1.js", "script2.js", "script3.js"]}, header: %{css: ["style1.css", "style2.css"], meta: "tags", title: "test"}}
+    assert decoded == %{"body" => %{"h1" => "test", "h3" => "testing"}, "footer" => %{"js" => ["script1.js", "script2.js", "script3.js"]}, "header" => %{"css" => ["style1.css", "style2.css"], "meta" => "tags", "title" => "test"}}
   end
 end
